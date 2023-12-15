886cddb22ccfc20e474f8c702232893a9969b99e
JeongEEE <tbtbtb156@naver.com>
2023-12-15 14:45:22 +0900

    CRUD

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const { default: mongoose } = require('mongoose')
 const { join } = require('path')
 const User = require('./models/users.model')
 const passport = require('passport')
+const bodyParser = require('body-parser')
 const app = express()
 require('dotenv').config()
 const cookieSession = require('cookie-session')
@@ -10,6 +11,8 @@ const config = require('config')
 const serverConfig = config.get('server')
 const mainRouter = require('./routes/main.router')
 const usersRouter = require('./routes/users.router')
+const productsRouter = require('./routes/product.router')
+const port = 4000
 
 const cookieEncryptionKey = process.env.COOKIE_ENCRYPTION_KEY
 app.use(
@@ -39,6 +42,8 @@ require('./config/passport')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // view engine setup
 app.set('views', join(__dirname, 'views'))
@@ -56,10 +61,15 @@ mongoose
 
 app.use('/static', express.static(join(__dirname, 'public')))
 
+app.use((error, req, res, next) => {
+  // error 처리기
+  res.json({ message: error.message })
+})
+
 app.use('/', mainRouter)
 app.use('/auth', usersRouter)
+app.use('/products', productsRouter)
 
-const port = 4000
 app.listen(port, () => {
   console.log('Server Start - Port =', port)
 })
